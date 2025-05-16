57d365d757a2558418f8fa89c79cd79d572a898e
dwiki <dwikis17@gmail.com>
2025-05-16 14:38:31 +0800

    wip

---
+++ ADA_C2_MomoRun/Scene/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ADA_C2_MomoRun
+//
+//  Created by Dwiki on 16/05/25.
+//
+
+import Foundation
+import SpriteKit
+
+
+final class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        size = view.frame..sizeter
+    }
+}
