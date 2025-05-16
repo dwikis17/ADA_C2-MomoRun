c2e20f06be16e09849a80cf25320b0ed5fc70c71
dwiki <dwikis17@gmail.com>
2025-05-16 13:55:21 +0800

    Initial Commit

---
+++ ADA_C2_MomoRun/ADA_C2_MomoRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADA_C2_MomoRunApp.swift
+//  ADA_C2_MomoRun
+//
+//  Created by Dwiki on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ADA_C2_MomoRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADA_C2_MomoRun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADA_C2_MomoRun
+//
+//  Created by Dwiki on 16/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
